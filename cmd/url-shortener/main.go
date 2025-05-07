afc80b85d91a4000fa4b10b28fb340775032aa33
monkstrife <drobilko2002@mail.ru>
2025-05-07 05:57:24 +0300

    Первый комит, инициализировал конфигураци, логер, а также базу данных

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
+
+	_ "github.com/mattn/go-sqlite3" // init sqlite3 driver
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
+func main() {
+	// TODO: init config: cleanvenv
+	cfg := config.MustLoad()
+
+	//fmt.Println(cfg)
+
+	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
+
+	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
